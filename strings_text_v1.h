7a7b361061771aa60568a8306110b44d5af7dab6
Johan Kämpe <kampe.johan@gmail.com>
2016-10-18 08:43:12 +0200

    Added missing declaration of TTS function

--- strings_text_v1.h
+++ strings_text_v1.h
@@ -3,8 +3,10 @@
 #include <string.h>
 #include <ctype.h>
 
+//Funktionsdeklarationer
 _Bool removeNewLine(char[]);
 void clearBuffer(void);
 void clearBufferFil(FILE *f);
 _Bool radInput(char[], int);
 _Bool textfilTillString(char[], int, FILE *f);
+_Bool TTS(char *a, int n, FILE *f);
