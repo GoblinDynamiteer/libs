b293918e348659ff5ca70279476c4950360ebf21
Johan Kämpe <kampe.johan@gmail.com>
2017-01-12 20:52:13 +0100

    Geometry calculations library

--- lib/libgeo.c
+++ lib/libgeo.c
@@ -2,7 +2,35 @@
  * libgeo.c
  *
  *  Created on: 12 jan. 2017
- *      Author: kampe
+ *      Author: Johan Kämpe
  */
 
+#include "libgeo.h"
 
+/*	 Calculate distance between two coordinates	*/
+double calculateDistance(int x1, int x2, int y1, int y2){
+	 double calc =
+		sqrt(
+		pow((float)x1 - (float)x2, 2) +
+		pow((float)y1 - (float)y2, 2)
+	);
+	return calc;
+}
+
+int deltaY(int x1, int x2){
+	return fabs(x1 - x2);
+}
+
+int deltaX(int y1, int y2){
+	return fabs(y1 - y2);
+}
+
+/*	 Angle from opposite and adjacent sides of a triangle	*/
+double angleArcTan(double o, double a){
+	return atan (o / a);
+}
+
+/*	 Converts radians to degrees	*/
+double radToDeg(double rads){
+	return rads * 180.0 / M_PI;
+}
--- lib/libgeo.h
+++ lib/libgeo.h
@@ -8,6 +8,12 @@
 #ifndef LIB_LIBGEO_H_
 #define LIB_LIBGEO_H_
 
+#include <math.h>
 
+double calculateDistance(int x1, int x2, int y1, int y2);
+int deltaY(int x1, int x2);
+int deltaX(int y1, int y2);
+double angleArcTan(double o, double a);
+double radToDeg(double rads);
 
 #endif /* LIB_LIBGEO_H_ */
