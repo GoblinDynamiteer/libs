e169e5d0272a6b36f94e32ea9e84969f7b7163cb
Johan Kämpe <kampe.johan@gmail.com>
2017-01-12 08:13:21 +0100

    Geomery calculations library

---
+++ lib/libgeo.c
@@ -0,0 +1,8 @@
+/*
+ * libgeo.c
+ *
+ *  Created on: 12 jan. 2017
+ *      Author: kampe
+ */
+
+
---
+++ lib/libgeo.h
@@ -0,0 +1,13 @@
+/*
+ * libgeo.h
+ *
+ *  Created on: 12 jan. 2017
+ *      Author: kampe
+ */
+
+#ifndef LIB_LIBGEO_H_
+#define LIB_LIBGEO_H_
+
+
+
+#endif /* LIB_LIBGEO_H_ */
