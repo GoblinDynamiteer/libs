4ead903cbe599c169d50a3ee6a6b917461473d9e
Johan Kämpe <kampe.johan@gmail.com>
2017-03-12 13:26:53 +0100

    Bitwise op lib

---
+++ lib/libbitop.c
@@ -0,0 +1,56 @@
+/*   Binary operations
+
+    Johan Kämpe */
+
+#include "libbitop.h"
+
+/*   Returns bit value at position 'bit'  */
+int checkBitAtPosition(int num, int bit){
+  int a = 0x0001 << bit-1; //Assumes the first bit starts at 1
+  return ((num & a) > 0);
+}
+
+/*   For binary output  */
+void printBinary(int num){
+  int spacer = 3;
+  for(int i = NUM_BITS; i > 0; i--){
+    printf("%i", checkBitAtPosition(num, i));
+    if(!spacer--){
+      printf(" ");
+      spacer = 3;
+    }
+  }
+}
+
+/*  Prints "#" at bit position, to be used with printBinary()  */
+void printPositionMarker(int num){
+  int spacer = 3;
+  for(int i = 1; i < NUM_BITS; i++){
+    printf(" ");
+    if(!spacer--){
+      printf(" ");
+      spacer = 3;
+    }
+    if(i == NUM_BITS - num){
+      break;
+    }
+  }
+  printf("#");
+}
+
+/*  Set bit at specific position to 1/0 */
+unsigned int setBit(unsigned int num, int bit, int value){
+  if(value){ //set bit to 1
+    /*   Shift in 1 to correct bit position to set to 1
+        and OR with num, the bit at postion 'bit' will be 1 */
+    num = num | (0x0001 << bit-1);
+  }
+  else{ //set bit to 0
+    /*   Shift in 1 to correct bit position to set to 0,
+        inverse all digits, and AND with num, the bit at
+        position 'bit' will be 0 */
+    num = num & ~(0x0001 << bit-1);
+  }
+  /*   Returns new number  */
+  return num;
+}
---
+++ lib/libbitop.h
@@ -0,0 +1,19 @@
+/*   Binary operations
+
+    Johan Kämpe */
+
+#ifndef LIBBITOP_H
+#define LIBBITOP_H
+
+#include <stdio.h> //printf
+
+/*  Bits in int (8 bits per byte)  */
+#define NUM_BITS sizeof(int) * 8
+
+/*  Functions in libbitop.c   */
+int checkBitAtPosition(int num, int bit);
+void printBinary(int hex);
+void printPositionMarker(int num);
+unsigned int setBitAtPosition(unsigned int num, int bit, int value);
+
+#endif
