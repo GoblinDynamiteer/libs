491eb36b2f8cd3a7b2237070a177a7732c324c6c
Johan Kämpe <kampe.johan@gmail.com>
2017-06-18 17:46:00 +0200

    Added functions and descriptions

--- lib/rgb/blinkm.c
+++ lib/rgb/blinkm.c
@@ -8,7 +8,14 @@
 
 #include "blinkm.h"
 
-uint8_t BlinkM_SetColor(uint8_t pwm_red, uint8_t pwm_green, uint8_t pwm_blue){
+/*   BlinkM_SetColor
+     Set RGB color of BlinkM
+     Parameters
+        pwm_red:    Red value
+        pwm_green:  Green value
+        pwm_blue:   Blue value
+     */
+void BlinkM_SetColor(uint8_t pwm_red, uint8_t pwm_green, uint8_t pwm_blue){
     uint8_t data_buffer[4] = {
         BLINKM_CMD_SETCOLOR,
         pwm_red,
@@ -16,4 +23,60 @@ uint8_t BlinkM_SetColor(uint8_t pwm_red, uint8_t pwm_green, uint8_t pwm_blue){
         pwm_blue
 	};
     I2C_SendData(BLINKM_ADDRESS, data_buffer, 4);
+}
+
+/*   BlinkM_FadeToColor
+     Fade to RGB color, from current color
+     Parameters
+        pwm_red:    Red value
+        pwm_green:  Green value
+        pwm_blue:   Blue value
+     */
+void BlinkM_FadeToColor(uint8_t pwm_red, uint8_t pwm_green, uint8_t pwm_blue){
+    uint8_t data_buffer[4] = {
+        BLINKM_CMD_FADETOCOLOR,
+        pwm_red,
+        pwm_green,
+        pwm_blue
+	};
+    I2C_SendData(BLINKM_ADDRESS, data_buffer, 4);
+}
+
+/*   BlinkM_SetFadeSpeed
+     Set fade speed color for scripts and fade commands
+     Parameters
+        speed:  Fade speed value between 1 and 255
+     Notes
+        A speed value of 0 is invalid, 255 is used for instant
+     */
+void BlinkM_SetFadeSpeed(uint8_t speed){
+    uint8_t data_buffer[2] = {
+        BLINKM_CMD_SETFADESPEED,
+        speed,
+	};
+    I2C_SendData(BLINKM_ADDRESS, data_buffer, 2);
+}
+
+/*   BlinkM_PlayScript
+     Play BlinkM script
+     Parameters
+        script_number:  Script id number
+        repeat:         Number of times to repeat script, 0 for endless
+     */
+void BlinkM_PlayScript(uint8_t script_number, uint8_t repeat){
+    uint8_t data_buffer[4] = {
+        BLINKM_CMD_STARTSCRIPT,
+        script_number,
+        repeat,
+        BLINKM_SETTING_SCRIPT_FIRST_LINE_START
+    };
+    I2C_SendData(BLINKM_ADDRESS, data_buffer, 4);
+}
+
+/*   BlinkM_PlayScript
+     Stop any running BlinkM script
+     */
+void BlinkM_StopScript(void){
+    uint8_t data = BLINKM_CMD_STOPSCRIPT;
+    I2C_SendData(BLINKM_ADDRESS, &data, 1);
 }
--- lib/rgb/blinkm.h
+++ lib/rgb/blinkm.h
@@ -15,10 +15,26 @@
 #define BLINKM_ADDRESS_ALTERNATIVE 0x12
 
 #define BLINKM_CMD_STOPSCRIPT 0x6F
+#define BLINKM_CMD_STARTSCRIPT 0x70
 #define BLINKM_CMD_SETCOLOR 0x6E
+#define BLINKM_CMD_FADETOCOLOR 0x63
 #define BLINKM_CMD_SETFADESPEED 0x66
-#define BLINKM_CMD_SETCOLORWITHFADE 0x63
 
-uint8_t BlinkM_SetColor(uint8_t pwm_red, uint8_t pwm_green, uint8_t pwm_blue);
+#define BLINKM_SETTING_SCRIPT_FIRST_LINE_START 0x00
+
+#define BLINKM_SCRIPT_PREDEFINED_MOOD_LIGHT 11
+#define BLINKM_SCRIPT_PREDEFINED_VIRTUAL_CANDLE 12
+#define BLINKM_SCRIPT_PREDEFINED_WATER_REFLECTIONS 13
+#define BLINKM_SCRIPT_PREDEFINED_OLD_NEON 14
+#define BLINKM_SCRIPT_PREDEFINED_THE_SEASONS 15
+#define BLINKM_SCRIPT_PREDEFINED_THUNDERSTORM 16
+#define BLINKM_SCRIPT_PREDEFINED_STOP_LIGHT 17
+#define BLINKM_SCRIPT_PREDEFINED_SOS 18
+
+void BlinkM_SetColor(uint8_t pwm_red, uint8_t pwm_green, uint8_t pwm_blue);
+void BlinkM_FadeToColor(uint8_t pwm_red, uint8_t pwm_green, uint8_t pwm_blue);
+void BlinkM_SetFadeSpeed(uint8_t speed);
+void BlinkM_PlayScript(uint8_t script_number, uint8_t repeat);
+void BlinkM_StopScript(void);
 
 #endif
